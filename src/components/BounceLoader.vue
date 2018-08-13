687150e1b45fc54c85d5ac10ff307ec8fbc972dd
Carly Darcher <casdarcher@gmail.com>
2018-08-13 12:02:04 -0700

    Moved state list to own page

--- src/components/BounceLoader.vue
+++ src/components/BounceLoader.vue
@@ -1,39 +1,38 @@
 <template>
-<div class="v-spinner" v-show="loading">
-    <div class="v-bounce v-bounce1" v-bind:style="spinnerBasicStyle">
-    <div class="v-bounce v-bounce2" v-bind:style="spinnerStyle">
-    </div><div class="v-bounce v-bounce3" v-bind:style="spinnerStyle">
-    </div></div>
-  </div>
+    <div v-bind:style="styles" class="spinner spinner--double-bounce">
+      <div class="double-bounce1" v-bind:style="bounceStyle"></div>
+      <div class="double-bounce2" v-bind:style="bounceStyle"></div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "BounceLoader",
+  
+  name: 'BounceLoader',
 
   props: {
     loading: {
       type: Boolean,
       default: true
     },
-    color: {
+    color: { 
       type: String,
-      default: "#5dc596"
+      default: '#5dc596'
     },
     size: {
       type: String,
-      default: "60px"
+      default: '60px'
     },
     margin: {
       type: String,
-      default: "2px"
+      default: '2px'
     },
     radius: {
       type: String,
-      default: "100%"
+      default: '100%'
     }
   },
-  data() {
+  data () {
     return {
       spinnerStyle: {
         backgroundColor: this.color,
@@ -41,63 +40,82 @@ export default {
         width: this.size,
         borderRadius: this.radius,
         opacity: 0.6,
-        position: "absolute",
+        position: 'absolute',
         top: 0,
         left: 0
       }
-    };
+    }
   },
   computed: {
-    spinnerBasicStyle() {
+    spinnerBasicStyle () {
       return {
         height: this.size,
         width: this.size,
-        position: "relative"
-      };
+        position: 'relative'
+      }
     }
   }
-};
+
+}
 </script>
 
 <style>
-.v-spinner .v-bounce {
+
+.v-spinner .v-bounce
+{
+          
 }
 
-.v-spinner .v-bounce2 {
-  -webkit-animation: v-bounceStretchDelay 2s 1s infinite ease-in-out;
-  animation: v-bounceStretchDelay 2s 1s infinite ease-in-out;
-  -webkit-animation-fill-mode: both;
-  animation-fill-mode: both;
+.v-spinner .v-bounce1
+{
 }
 
-.v-spinner .v-bounce3 {
-  -webkit-animation: v-bounceStretchDelay 2s 0s infinite ease-in-out;
-  animation: v-bounceStretchDelay 2s 0s infinite ease-in-out;
-  -webkit-animation-fill-mode: both;
-  animation-fill-mode: both;
+.v-spinner .v-bounce2
+{
+
+    -webkit-animation: v-bounceStretchDelay 2s 1s infinite ease-in-out;
+            animation: v-bounceStretchDelay 2s 1s infinite ease-in-out;
+    -webkit-animation-fill-mode: both;
+            animation-fill-mode: both;
 }
 
-@-webkit-keyframes v-bounceStretchDelay {
-  0%,
-  100% {
-    -webkit-transform: scale(0);
-    transform: scale(0);
-  }
-  50% {
-    -webkit-transform: scale(1);
-    transform: scale(1);
-  }
+.v-spinner .v-bounce3
+{
+    -webkit-animation: v-bounceStretchDelay 2s 0s infinite ease-in-out;
+            animation: v-bounceStretchDelay 2s 0s infinite ease-in-out;
+    -webkit-animation-fill-mode: both;
+            animation-fill-mode: both;
 }
 
-@keyframes v-bounceStretchDelay {
-  0%,
-  100% {
-    -webkit-transform: scale(0);
-    transform: scale(0);
-  }
-  50% {
-    -webkit-transform: scale(1);
-    transform: scale(1);
-  }
+@-webkit-keyframes v-bounceStretchDelay
+{
+    0%,
+    100%
+    {
+        -webkit-transform: scale(0);
+                transform: scale(0);
+    }
+    50%
+    { 
+        -webkit-transform: scale(1.0);
+                transform: scale(1.0);
+    
+    }
+}
+
+@keyframes v-bounceStretchDelay
+{
+    0%,
+    100%
+    {
+        -webkit-transform: scale(0);
+                transform: scale(0);
+    }
+    50%
+    { 
+        -webkit-transform: scale(1.0);
+                transform: scale(1.0);
+    
+    }
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,11 +9,10 @@
               Use this quick search tool to bring up a list of schools in the state/territory of your choice</p> 
               <b-form v-on:submit.prevent="findSchools"> 
                 <select v-model="selected">
-                  <option v-for="state in states" v-bind:value="state.abbreviation">
-                    {{ state.name }}
-                  </option>
+                  <option disabled value="">Choose a location</option>
+                  <option v-for="state in states" v-bind:value="state.abbreviation"> {{ state.name }} </option>
                 </select>
-                  <input class="button" type="submit"  v-bind:to="{ name: 'CollegeList' }"value="View Schools">
+                  <input class="button" type="submit"  v-bind:to="{ name: 'CollegeList' } "value="View Schools">
             </b-form> 
                 <!-- <div class="schoolCard" v-if="schools && schools.length > 0">
                   <b-card :title=item"['school.name']"
@@ -31,7 +30,7 @@
                 <!-- List of Schools -->  
                 <div class="school-list">
                     <ul class="schools" v-if="schools && schools.length > 0">
-                      <transition-group name="fadeRight" tag="div" class="school-list" appear>
+                      <transition-group name="slideRight" tag="div" class="school-list" appear>
                       <li v-for="(item,index) in schools" :key="index">
                         <spinner v-if="showLoading"></spinner>  
                           <h3>{{ item["school.name"]}}</h3><br>
@@ -49,274 +48,31 @@
 <script>
 import axios from "axios";
 import BounceLoader from "@/components/BounceLoader";
+import states from "../states.js";
 // Note: vue2-animate is added using the require statement because it is a CSS file
 require('vue2-animate/dist/vue2-animate.min.css');
 
 export default {
   name: "Home",
-  components: {
-  "spinner": BounceLoader
-  },
-
-
   data() {
     return {
       selectedState: "",
       showLoading: false,
       schools: [],
       selected: "",
-      states: [
-        {
-          name: "Alabama",
-          abbreviation: "AL"
-        },
-        {
-          name: "Alabama",
-          abbreviation: "AL"
-        },
-        {
-          name: "Alaska",
-          abbreviation: "AK"
-        },
-        {
-          name: "American Samoa",
-          abbreviation: "AS"
-        },
-        {
-          name: "Arizona",
-          abbreviation: "AZ"
-        },
-        {
-          name: "Arkansas",
-          abbreviation: "AR"
-        },
-        {
-          name: "California",
-          abbreviation: "CA"
-        },
-        {
-          name: "Colorado",
-          abbreviation: "CO"
-        },
-        {
-          name: "Connecticut",
-          abbreviation: "CT"
-        },
-        {
-          name: "Delaware",
-          abbreviation: "DE"
-        },
-        {
-          name: "District Of Columbia",
-          abbreviation: "DC"
-        },
-        {
-          name: "Federated States Of Micronesia",
-          abbreviation: "FM"
-        },
-        {
-          name: "Florida",
-          abbreviation: "FL"
-        },
-        {
-          name: "Georgia",
-          abbreviation: "GA"
-        },
-        {
-          name: "Guam",
-          abbreviation: "GU"
-        },
-        {
-          name: "Hawaii",
-          abbreviation: "HI"
-        },
-        {
-          name: "Idaho",
-          abbreviation: "ID"
-        },
-        {
-          name: "Illinois",
-          abbreviation: "IL"
-        },
-        {
-          name: "Indiana",
-          abbreviation: "IN"
-        },
-        {
-          name: "Iowa",
-          abbreviation: "IA"
-        },
-        {
-          name: "Kansas",
-          abbreviation: "KS"
-        },
-        {
-          name: "Kentucky",
-          abbreviation: "KY"
-        },
-        {
-          name: "Louisiana",
-          abbreviation: "LA"
-        },
-        {
-          name: "Maine",
-          abbreviation: "ME"
-        },
-        {
-          name: "Marshall Islands",
-          abbreviation: "MH"
-        },
-        {
-          name: "Maryland",
-          abbreviation: "MD"
-        },
-        {
-          name: "Massachusetts",
-          abbreviation: "MA"
-        },
-        {
-          name: "Michigan",
-          abbreviation: "MI"
-        },
-        {
-          name: "Minnesota",
-          abbreviation: "MN"
-        },
-        {
-          name: "Mississippi",
-          abbreviation: "MS"
-        },
-        {
-          name: "Missouri",
-          abbreviation: "MO"
-        },
-        {
-          name: "Montana",
-          abbreviation: "MT"
-        },
-        {
-          name: "Nebraska",
-          abbreviation: "NE"
-        },
-        {
-          name: "Nevada",
-          abbreviation: "NV"
-        },
-        {
-          name: "New Hampshire",
-          abbreviation: "NH"
-        },
-        {
-          name: "New Jersey",
-          abbreviation: "NJ"
-        },
-        {
-          name: "New Mexico",
-          abbreviation: "NM"
-        },
-        {
-          name: "New York",
-          abbreviation: "NY"
-        },
-        {
-          name: "North Carolina",
-          abbreviation: "NC"
-        },
-        {
-          name: "North Dakota",
-          abbreviation: "ND"
-        },
-        {
-          name: "Northern Mariana Islands",
-          abbreviation: "MP"
-        },
-        {
-          name: "Ohio",
-          abbreviation: "OH"
-        },
-        {
-          name: "Oklahoma",
-          abbreviation: "OK"
-        },
-        {
-          name: "Oregon",
-          abbreviation: "OR"
-        },
-        {
-          name: "Palau",
-          abbreviation: "PW"
-        },
-        {
-          name: "Pennsylvania",
-          abbreviation: "PA"
-        },
-        {
-          name: "Puerto Rico",
-          abbreviation: "PR"
-        },
-        {
-          name: "Rhode Island",
-          abbreviation: "RI"
-        },
-        {
-          name: "South Carolina",
-          abbreviation: "SC"
-        },
-        {
-          name: "South Dakota",
-          abbreviation: "SD"
-        },
-        {
-          name: "Tennessee",
-          abbreviation: "TN"
-        },
-        {
-          name: "Texas",
-          abbreviation: "TX"
-        },
-        {
-          name: "Utah",
-          abbreviation: "UT"
-        },
-        {
-          name: "Vermont",
-          abbreviation: "VT"
-        },
-        {
-          name: "Virgin Islands",
-          abbreviation: "VI"
-        },
-        {
-          name: "Virginia",
-          abbreviation: "VA"
-        },
-        {
-          name: "Washington",
-          abbreviation: "WA"
-        },
-        {
-          name: "West Virginia",
-          abbreviation: "WV"
-        },
-        {
-          name: "Wisconsin",
-          abbreviation: "WI"
-        },
-        {
-          name: "Wyoming",
-          abbreviation: "WY"
-        }
-      ]
+      states: states,
     };
   },
 
-
+  components: {
+  'spinner': BounceLoader
+  },
 
   // Pull information from API
   methods: {
     findSchools: function() {
       // Show spinner when API request begins
-      this.showLoader = true;
+      this.showLoading = true;
       axios.get('https://api.data.gov/ed/collegescorecard/v1/schools.json', {
       params: {
         api_key: 'd9xeq99pO8xRBij92yBUzmSIPR1P6v4RDAyQN53X',
@@ -327,7 +83,7 @@ export default {
       })
       .then(response => {
       // Turn off spinner
-      this.showLoader = false;
+      this.showLoading = false;
       this.schools = response.data.results
       });
     }
@@ -397,7 +153,4 @@ hr {
     border-width: .5px;
 
 }
-
-
-
 </style>
