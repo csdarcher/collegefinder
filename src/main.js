36e6219c24654e1021863e14bd21f9d4d1b494f3
Carly Darcher <casdarcher@gmail.com>
2018-09-01 09:10:09 -0700

    Added analytics info

--- src/main.js
+++ src/main.js
@@ -6,10 +6,19 @@ import router from './Router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGraduationCap, faGlasses, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueAnalytics from 'vue-analytics'
 
 library.add(faGraduationCap, faSearch)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-125015556-1',
+  autotracking: {
+    exception: true
+  },
+  router
+})
 
 
 /* eslint-disable no-new */
